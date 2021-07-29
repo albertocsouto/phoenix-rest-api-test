ec4dc798c839b02e00ea286ecdf8b29e8c841585
Alberto Caamaño Souto <albertocsouto@gmail.com>
2021-07-29 21:35:38 +0200

    Updated postgres port for travis

--- config/travis.exs
+++ config/travis.exs
@@ -6,6 +6,7 @@ config :test_api, TestApi.Repo,
   password: "",
   database: "test_api_dev",
   hostname: "localhost",
+  port: 5432,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
